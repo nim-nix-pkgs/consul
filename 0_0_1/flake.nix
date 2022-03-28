39f7057c60cc97d7b825464da8462f9f858f1b7a
hugosenari <hugosenari@gmail.com>
2022-03-27 21:48:46 -0300

    chore: re index consul

--- 0_0_1/flake.nix
+++ 0_0_1/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-consul-0_0_1.ref   = "refs/tags/0.0.1";
   inputs.src-consul-0_0_1.owner = "makingspace";
   inputs.src-consul-0_0_1.repo  = "nim_consul";
-  inputs.src-consul-0_0_1.dir   = "";
   inputs.src-consul-0_0_1.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
